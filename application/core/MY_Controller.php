63b037c8729f7b90896adb474e97dbf2faff93e4
Felipe Reno Oliveira Pisa <felipe@cysman.com>
2014-01-18 12:49:05 +0000

    Update MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,47 +1,61 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
-  protected $layout = 'application';
+class MY_Controller extends CI_Controller
+{
+    protected $_layout = 'application';
 
-  protected $stylesheets = array(
-    'application'
-  );
-  protected $javascripts = array(
-    'application'
-  );
+    protected $stylesheets = array('application');
 
-  protected function render($content, $layout = NULL) {
-    $view_data = array(
-      'content' => $content,
-      'css' => $this->get_stylesheets(),
-      'js' => $this->get_javascripts()
-    );
+    protected $javascripts = array('application');
 
-    $template = 'layouts/'.($layout? : $this->layout);
-    $this->load->view($template, $view_data);
-  }
-
-  protected function get_stylesheets() {
-    return $this->stylesheets;
-  }
-
-  protected function get_javascripts() {
-    return $this->javascripts;
-  }
-
-  protected function css($file_list){
-    if (!is_array($file_list)){
-      $file_list = array($file_list);
+    public function setLayout($layout)
+    {
+        $this->_layout = $layout;
     }
 
-    $this->stylesheets = array_merge($this->stylesheets, $file_list);
-  }
+    public function json()
+    {
 
-  protected function js($file_list){
-    if (!is_array($file_list)){
-      $file_list = array($file_list);
     }
 
-    $this->javascripts = array_merge($this->javascripts, $file_list);
-  }
+    public function view($namespaced_view, $data = array())
+    {
+        $view = strtolower(get_called_class()).'/'.$namespaced_view;
+
+        $template = array(
+          'content' => $this->load->view($view, $data, true),
+          'css' => $this->get_stylesheets(),
+          'js' => $this->get_javascripts()
+        );
+
+        $this->load->view('layouts/'.$this->_layout, $template);
+    }
+
+    protected function get_stylesheets()
+    {
+        return $this->stylesheets;
+    }
+
+    protected function get_javascripts()
+    {
+        return $this->javascripts;
+    }
+
+    protected function css($file_list)
+    {
+        if (!is_array($file_list)){
+            $file_list = array($file_list);
+        }
+
+        $this->stylesheets = array_merge($this->stylesheets, $file_list);
+    }
+
+    protected function js($file_list)
+    {
+        if (!is_array($file_list)){
+            $file_list = array($file_list);
+        }
+
+        $this->javascripts = array_merge($this->javascripts, $file_list);
+    }
 }
