44ca4653d5f4a7e88d52b87cbc9977c626a62f93
Felipe Reno Oliveira Pisa <felipe@cysman.com>
2014-01-19 12:34:28 +0000

    Fix views

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,8 +2,15 @@
 
 class Home extends MY_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('home_model');
+    }
+
 	public function index()
 	{
-		$this->view('index');
+		$this->view('index', $this->home_model);
 	}
 }
